23fdd152f4a92c4fc539b4698aa4cbdfe4b52510
IrynaZubarieva <iphonezub1@gmail.com>
2022-01-31 14:11:30 +0200

    д/з7 js

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const imagesContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+
+imagesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+imagesContainer.addEventListener('click', onImagesContainerClick);
+
+function createImageCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `      
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}" >
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+      </div>
+      `
+    }).join('');
+}
+
+function onImagesContainerClick(e) {
+    const isGalleryItemEl = e.target.classList.contains('gallery__image');
+    const imageSrc = e.target.dataset.source;
+    e.preventDefault();
+    if (!isGalleryItemEl) {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${imageSrc}" width="800" height="600">`, {
+        onShow: () => {
+            window.addEventListener("keydown", closeByEscButton)
+        },
+        onClose: () => {
+            window.removeEventListener("keydown", closeByEscButton)
+        },
+    });
+   
+function closeByEscButton(e) {
+        if (e.code === 'Escape') {
+            instance.close();
+        };
+    };
+    instance.show();
+}
+console.log(galleryItems);
---
+++ js/02-lightbox.js
@@ -0,0 +1,28 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const imagesContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+imagesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+imagesContainer.addEventListener('click', onImagesContainerClick);
+
+function createImageCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `      
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+      `
+    }).join('');
+}
+
+function onImagesContainerClick(e) {
+    const isGalleryItemEl = e.target.classList.contains('gallery__image');
+    const imageSrc = e.target.dataset.source;
+    e.preventDefault();
+    if (!isGalleryItemEl) {
+        return;
+    }
+}
+let lightbox = new SimpleLightbox('.gallery__item', {captionsData: 'alt', captionDelay: 250, });
+
+console.log(galleryItems);
